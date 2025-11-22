5f7bd7bb67ab62ff99bc6ad124100080906e47a2
MarcosG2003 <marcosgarnica12@gmail.com>
2025-11-21 22:17:37 -0600

    Got the total number of words printed

--- main.py
+++ main.py
@@ -3,8 +3,21 @@ def get_book_text(file_path):
         file_contents = f.read()
     return file_contents
 
+
+def word_count(text_from_book):
+    words = text_from_book.split()
+    num_of_words = len(words)
+    return num_of_words
+
+
+
 def main():
     relative_file_path = "books/frankenstein.txt"
-    print(get_book_text(relative_file_path))
+    #print(get_book_text(relative_file_path))
+    text = get_book_text(relative_file_path)
+    num_words = word_count(text)
+    print(f"Found {num_words} total words")
+    
+
 
 main()
