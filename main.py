8a9f6bb993428ca7041e6453ef821438827dd55d
MarcosG2003 <marcosgarnica12@gmail.com>
2025-11-21 22:03:08 -0600

    Defined get_book_text and main, tested with frankenstein.txt

---
+++ main.py
@@ -0,0 +1,10 @@
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+def main():
+    relative_file_path = "books/frankenstein.txt"
+    print(get_book_text(relative_file_path))
+
+main()
