75e95dae4e7073e1c1888c91f824ea4a95169921
MarcosG2003 <marcosgarnica12@gmail.com>
2025-11-22 00:25:26 -0600

    Included system arguments, and mobydick and prideandprejudice books as well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import get_num_words
 from stats import get_num_characters
 from stats import char_count
+import sys
 
 
 def get_book_text(file_path):
@@ -11,8 +12,22 @@ def get_book_text(file_path):
 
 
 def main():
-    relative_file_path = "books/frankenstein.txt"
+    #relative_file_path = "books/frankenstein.txt"
+
     #print(get_book_text(relative_file_path))
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    relative_file_path = sys.argv[1]
+
+    
+
+
+
+
+
     text = get_book_text(relative_file_path)
     num_words = get_num_words(text)
     letters = get_num_characters(text)
@@ -25,9 +40,9 @@ def main():
     print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    for dict in Character_count_list:
-        char = dict["char"]
-        num = dict["num"]
+    for entry in Character_count_list:
+        char = entry["char"]
+        num = entry["num"]
         if char.isalpha():
             print(f"{char}: {num}")
     
