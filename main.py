8b111fd540f1282a4a11df80a7519e9b11af9969
MarcosG2003 <marcosgarnica12@gmail.com>
2025-11-21 22:59:31 -0600

    Added in get_num_character to get the total number of characters that appear in the book

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_num_characters
 
 
 def get_book_text(file_path):
@@ -13,7 +14,13 @@ def main():
     #print(get_book_text(relative_file_path))
     text = get_book_text(relative_file_path)
     num_words = get_num_words(text)
+    letters = get_num_characters(text)
+
+
+
     print(f"Found {num_words} total words")
+    print(letters)
+
     
 
 
--- stats.py
+++ stats.py
@@ -2,3 +2,27 @@ def get_num_words(text_from_book):
     words = text_from_book.split()
     num_of_words = len(words)
     return num_of_words
+
+#Add a new function
+#that takes the text from the book as a string
+#and returns the number of times each character (including sysmbols and spaces)
+#appears in the string
+#Convert any character to lowercase using .lower() method
+#Use a dictionary of String -> Integer.
+#should look like this 
+#{'p': 6121, 'r': 20818, 'o': 25225, ...
+
+def get_num_characters(text_from_book):
+    text = text_from_book.lower()
+    
+    letter_count_dict = {}
+    
+
+    for char in text:
+        if char in letter_count_dict:
+            letter_count_dict[char] += 1
+        else:
+            letter_count_dict[char] = 1
+    return letter_count_dict
+
+
